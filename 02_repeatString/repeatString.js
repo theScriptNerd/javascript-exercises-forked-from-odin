5ad73ecc10887b388daa3c2a826bc5858fc54311
theOutcast43 <kokehinc@gmail.com>
2023-07-17 00:59:40 +0100

    done with practice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    if(times < 0) return 'ERROR';
+    let output = '';
+    for (i = 0; i < times; i++){
+        output += word;
+    }
+    return output;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    if (!Number.isInteger(first) || !Number.isInteger(last)) return 'ERROR';
+    if (last < 0 || first < 0) return 'ERROR';
+    if (first > last){
+        const temp = first;
+        first = last;
+        last = temp;
+    }
+    let init = 0;
+    for (i = first; i <= last; i++){
+        init += i;
+    }
+    return init;
 };
 
 // Do not edit below this line
